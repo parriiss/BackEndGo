0313699d01a3760e09823433c4c007b2dbef32a6
paris <bartzokn@gmail.com>
2018-07-16 16:37:30 +0300

    Add Pad Struct and the structure for storing all the Pads that are created

--- src/Controller/controllers.go
+++ src/Controller/controllers.go
@@ -9,6 +9,48 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+//--------PARIS-------New Pad----
+type Pad struct{
+ID string json:'id'
+Name string json:'name'
+Last_edited string json:'last_edited'
+Size int json:'size'
+}
+var PadMap=make(map[string]*Pad)
+
+
+func store_pad(w http.ResponseWriter ,r *http.Request, p httprouter.Params){
+PadMap[id]=&Pad{
+id,
+Name.
+Last_edited,
+Size,
+}
+
+
+}
+
+
+
+//-----------PARIS_END------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // controller for requests (methods)
 type Controller struct{}
 
