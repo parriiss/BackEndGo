9c35bf4772a1ad33dfa94848ae6992f0ced92968
paris <bartzokn@gmail.com>
2018-07-17 12:15:21 +0300

    model.go ready newPad

--- src/Controller/model.go
+++ src/Controller/model.go
@@ -1,40 +1,47 @@
-package model
+package main
 
 import (
-        "fmt"
-        "encoding/json"
-        "net/http"
-        "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"encoding/json"
+	"fmt"
 )
 
-
-
-
-
-
-//--------PARIS-------New Pad----
 type Pad struct{
-ID string json:'id'
-Name string json:'name'
-Last_edited string json:'last_edited'
-Size int json:'size'
+	ID string `json:"id"`
+	Name string `json:"name"`
 }
 var PadMap=make(map[string]*Pad)
 
 
-func store_pad(w http.ResponseWriter ,r *http.Request, p httprouter.Params){
-PadMap[id]=&Pad{
-id,
-Name.
-Last_edited,
-Size,
+func store_pad(w http.ResponseWriter ,r *http.Request, _ httprouter.Params){
+	fmt.Fprint(w,"Test1\n")
+
+	decoder := json.NewDecoder(r.Body)
+	var t Pad
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	PadMap[t.ID]=&Pad{
+		t.ID,
+		t.Name,
+
+	}
+
+	fmt.Println(PadMap["zxc23"].ID)
+
 }
 
 
+
+func main(){
+
+	router := httprouter.New()
+	router.POST("/",store_pad)
+
+	log.Fatal(http.ListenAndServe(":8080" ,router))
+
 }
-
-
-
-//-----------PARIS_END------------
-
 
