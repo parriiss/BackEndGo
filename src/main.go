fdee24a504896c547893d714c466c05eb7060f0e
Unknown <athineouc@gmail.com>
2018-07-20 15:58:20 +0300

    implementation of getPadHistory function
    return the whole history of a pad

--- src/main.go
+++ src/main.go
@@ -7,11 +7,10 @@ package main
 	for requests
 */
 
-
 /*
 	***IMPORTANT***
 	TODO:
-		~define better API endponts  
+		~define better API endponts
 			what requests are expected???
 
 		~need to keep content of notepad in file
@@ -25,11 +24,10 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"sort"
 	"sync"
 	"time"
-	"sort"
 )
-
 
 // map for saving possible out-of-order requsts
 var Saved_requests []Requests.Editor_req
@@ -46,8 +44,8 @@ func main() {
 	handleURLS(r)
 
 	// init global vars channel
-	/* 
-		possibly multiple go routines can be created for faster 
+	/*
+		possibly multiple go routines can be created for faster
 		request-handling/writing to file, if so can_write_to_file
 		mutex will come in handy and a mutex for slice access
 		is possibly necessary
@@ -56,15 +54,13 @@ func main() {
 
 	//close channel when done
 	defer Requests.CloseChannel()
-	
+
 	// fire up server
 	http.ListenAndServe(":8000", r)
 }
 
-
-
 /*
-	Assign functions to handle requests	
+	Assign functions to handle requests
 	~maybe URLS need to change??
 */
 func handleURLS(r *httprouter.Router) {
@@ -73,11 +69,10 @@ func handleURLS(r *httprouter.Router) {
 	// 	GET
 	r.GET("/OnlineEditor/About", c.About)
 
-	
-
 	// 	POST
 	// r.POST(<URL1> , <function>)
-	r.POST("/LoadFile",c.LoadFile)
+	r.POST("/LoadFile", c.LoadFile)
+	r.POST("/PadHistory", c.GetPadHistory)
 
 	// ....
 	// ...
@@ -97,13 +92,12 @@ func handleURLS(r *httprouter.Router) {
 
 }
 
-
-/* 
+/*
 	this is supposed to act as a go routine
-	running in the background receiving requests 
+	running in the background receiving requests
 	through the in channel,
-		
-	saves them and periodically(5sec, not many words can be written hence can be 
+
+	saves them and periodically(5sec, not many words can be written hence can be
 	lost if server goes down for some reason)
 	write into file serverside
 */
@@ -116,7 +110,7 @@ func Handle_Requests() {
 		r, ok := <-Requests.In
 		if ok {
 			SavedReq_Mux.Lock()
-			Saved_requests = append(Saved_requests , r)
+			Saved_requests = append(Saved_requests, r)
 			SavedReq_Mux.Unlock()
 			// right request has arrived
 		}
@@ -124,7 +118,7 @@ func Handle_Requests() {
 		/*
 			Timer to update files after 5secs
 		*/
-		if next_upd.After(time.Now()){
+		if next_upd.After(time.Now()) {
 			// next update will come after 5secs
 			next_upd = time.Now().Add(addDur(0, 0, 5))
 			serve_reqs()
@@ -141,10 +135,11 @@ func serve_reqs() {
 	/*
 		TODO:
 			***IMPORTANT***
-			
-			~Changes in file should be broadcasted to every
-				user connected to the editted notepad
-				(broadcast AFTER files changed)
+
+			~Changes in file should be boadcasted to every
+			implementation of getPadHistory function
+			user connected to the editted notepad
+			(broadcast AFTER files changed)
 
 	*/
 
@@ -153,58 +148,56 @@ func serve_reqs() {
 	SavedReq_Mux.Lock()
 
 	/*
-	Either:
- 		another serve_reqs called from handle routine
- 			has emptied slice and unlocked mutex
-	OR
-		no requests have arrived
+			Either:
+		 		another serve_reqs called from handle routine
+		 			has emptied slice and unlocked mutex
+			OR
+				no requests have arrived
 	*/
-	if len(Saved_requests) == 0{
+	if len(Saved_requests) == 0 {
 		SavedReq_Mux.Unlock()
 		return
 	}
 
-	// sort requests by time they were created so 
+	// sort requests by time they were created so
 	// editing in files can be done in the right order
 	sort.Sort(Requests.Oldest_First(Saved_requests))
 
 	// if request that I'm expecting is saved
 	//  possible to have multiple out of order
-	for _, v := range Saved_requests{
+	for _, v := range Saved_requests {
 		switch v.Req_type {
-			case Requests.Ins:
-				/*
-					Insert into file:
-						delete contents of file from
-						r.OffsetFrom till r.OffsetTo
-						and insert r.Value to 
-						r.OffsetFrom position
-				*/
-				
+		case Requests.Ins:
+			/*
+				Insert into file:
+					delete contents of file from
+					r.OffsetFrom till r.OffsetTo
+					and insert r.Value to
+					r.OffsetFrom position
+			*/
 
-			case Requests.Dlt:
-				/*
-					Delete from file:
-						delete contents of file from
-						r.OffsetFrom till r.OffsetTo
-				*/
-				
-			case Requests.Wr:
-				/*
-					write into file r.Value at position r.OffsetFrom:
-						paste: many chars to specific loc
-						inpt: one char to location
-				*/
-				
-			default:
-				fmt.Println("Unknown req, in serve reqs: ",v.Req_type)
-		}/*End of Switch*/
+		case Requests.Dlt:
+			/*
+				Delete from file:
+					delete contents of file from
+					r.OffsetFrom till r.OffsetTo
+			*/
+
+		case Requests.Wr:
+			/*
+				write into file r.Value at position r.OffsetFrom:
+					paste: many chars to specific loc
+					inpt: one char to location
+			*/
+
+		default:
+			fmt.Println("Unknown req, in serve reqs: ", v.Req_type)
+		} /*End of Switch*/
 	} /*End of for*/
 	SavedReq_Mux.Unlock()
-}  
+}
 
-
-func addDur(h,m,s int) (time.Duration){
-	return time.Hour * time.Duration(h) + time.Minute * time.Duration(m) +
-		time.Second * time.Duration(s)
+func addDur(h, m, s int) time.Duration {
+	return time.Hour*time.Duration(h) + time.Minute*time.Duration(m) +
+		time.Second*time.Duration(s)
 }
