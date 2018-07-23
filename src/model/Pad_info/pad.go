c8636f45593fe68a1f599cb15fd47dedc6a2d906
k0k0s <KokonozisManos@gmail.com>
2018-07-23 14:16:50 +0300

    Changed settings requests for error handling

---
+++ src/model/Pad_info/pad.go
@@ -0,0 +1,10 @@
+// pad.go
+
+package Pad
+
+//----------------------Options for Pad---NewPad(StorePad),Delete,Rename,EmptyDocument---------------------------
+type Pad_info struct{
+	ID string 	`json:"id"`
+	Name string 	`json:"name"`
+	Value string 	`json:"value"`
+}
